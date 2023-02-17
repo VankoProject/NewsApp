2651ead97eb35adb6f033fa99dbbee55266a6ebf
VankoProgect <ivan.green69@gmail.com>
2023-02-18 00:02:18 +0200

    added bottom menu and all fragments in navigation graph. Created ui only in splash screen.

---
+++ app/src/main/java/com/example/newsapp/presentation/search/SearchFragment.kt
@@ -0,0 +1,30 @@
+package com.example.newsapp.presentation.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.newsapp.databinding.FragmentSearchBinding
+
+
+class SearchFragment : Fragment() {
+
+
+    private var _binding: FragmentSearchBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
