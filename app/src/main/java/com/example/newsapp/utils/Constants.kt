ccdbabc96da35b7582a79756e32cfc490bc55baf
VankoProgect <ivan.green69@gmail.com>
2023-02-18 17:17:59 +0200

    provide retrofit get requests, create di for retrofit and build service models from json.

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.utils
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "5862c33bf15046088a4550985479670e"
+    }
+}
