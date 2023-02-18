1daf41814615bf5911250690e18d065cca8cb0a6
VankoProgect <ivan.green69@gmail.com>
2023-02-18 18:56:56 +0200

    create bd and mainViewModel

--- app/src/main/java/com/example/newsapp/presentation/main/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/presentation/main/MainFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.newsapp.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val mBinding get() = _binding!!
+
+    private val viewModel by viewModels<MainViewModel> ()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +23,11 @@ class MainFragment : Fragment() {
     ): View {
         _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.all
     }
 
     override fun onDestroy() {
