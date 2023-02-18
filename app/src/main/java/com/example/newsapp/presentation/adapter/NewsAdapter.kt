7b90a4aef371461323b112cddcd27fa2fa354f3d
VankoProgect <ivan.green69@gmail.com>
2023-02-18 22:50:18 +0200

    create RecyclerView and bind with data from Api

---
+++ app/src/main/java/com/example/newsapp/presentation/adapter/NewsAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.newsapp.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsapp.R
+import com.example.newsapp.models.Article
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+
+
+    inner class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val articleImage = view.findViewById<ImageView>(R.id.article_image)
+        val articleTitle = view.findViewById<TextView>(R.id.article_title)
+        val articleDate = view.findViewById<TextView>(R.id.article_date)
+    }
+
+    private val callback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, callback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        return NewsViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_article, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(holder.articleImage)
+            holder.articleImage.clipToOutline = true
+            holder.articleTitle.text = article.title
+            holder.articleDate.text = article.publishedAt
+
+        }
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+
+}
--- app/src/main/java/com/example/newsapp/presentation/main/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/presentation/main/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.example.newsapp.presentation.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.databinding.FragmentMainBinding
+import com.example.newsapp.presentation.adapter.NewsAdapter
+import com.example.newsapp.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,7 +19,8 @@ class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val mBinding get() = _binding!!
 
-    private val viewModel by viewModels<MainViewModel> ()
+    private val viewModel by viewModels<MainViewModel>()
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +32,36 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.all
+        initAdapter()
+        viewModel.newsLiveData.observe(viewLifecycleOwner) { responce ->
+            when (responce) {
+                is Resource.Success -> {
+                    mBinding.pagProgressBar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    mBinding.pagProgressBar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        Log.e("checkData", "MainFragment: error: ${it}" )
+                    }
+                }
+                is Resource.Loading -> {
+                    mBinding.pagProgressBar.visibility = View.VISIBLE
+
+                }
+            }
+        }
+
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        mBinding.newsAdapter.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
     override fun onDestroy() {
